176bcabc2f7921c8bdcc04522e975bb3725e7c44
Mario Luz <marionetoluz@gmail.com>
2021-02-09 11:54:53 -0300

    Initial Commit

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCsqfxOhFfDdTOJhOAj7Ptezd_svqrx0uk",
+    authDomain: "evento-5efd7.firebaseapp.com",
+    projectId: "evento-5efd7",
+    storageBucket: "evento-5efd7.appspot.com",
+    messagingSenderId: "35058482285",
+    appId: "1:35058482285:web:6839917e770162977c1364"
+  };
+  // Initialize Firebase
+ export default firebase.initializeApp(firebaseConfig);
